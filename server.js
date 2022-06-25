a3c494d5d960128dddc1778bbe1f8613db39d925
Mikekuney <96913841+Mikekuney@users.noreply.github.com>
2022-06-25 11:19:08 -0400

    added app.use line 17

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 const app = express();
 
 app.use(logger("dev"));
-
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+app.use(express.static("./"));
 
 mongoose.connect(MONGODB_URI, {
   useUnifiedTopology: true,
